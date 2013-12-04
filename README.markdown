84bb57e01d1c055cbc8f23813479847b8531830a
Bryan Traywick <bryan@railsmachine.com>
2013-12-04 16:04:24 -0500

    Initial commit.

---
+++ README.markdown
@@ -0,0 +1,75 @@
+# Moonshine::Omnipitr
+
+## Introduction
+
+Welcome to Moonshine::Omnipitr, a [Moonshine](http://github.com/railsmachine/moonshine) plugin for installing and managing [OmniPITR](https://github.com/omniti-labs/omnipitr). This plugin installs and configures OmniPITR on the PostgreSQL slave. It configures a cron job to backup the database daily and uploads the backup to Amazon S3.
+
+Here's some quick links:
+
+ * [Homepage](http://github.com/railsmachine/moonshine_omnipitr)
+ * [Issues](http://github.com/railsmachine/moonshine_omnipitr/issues) 
+ * [Wiki](http://github.com/railsmachine/moonshine_omnipitr/wiki) 
+ * [Mailing List](http://groups.google.com)
+ * Resources for using OmniPITR:
+   * [OmniPITR Homepage](https://github.com/omniti-labs/omnipitr)
+
+## Quick Start
+
+Moonshine::Omnipitr is installed as a Rails plugin:
+
+    # Rails 2.x.x
+    script/plugin install git://github.com/railsmachine/moonshine_omnipitr.git
+    # Rails 3.x.x
+    script/rails plugin install git://github.com/railsmachine/moonshine_omnipitr.git
+
+Once it's installed, you can include it in your manifest:
+
+    # app/manifests/database_manifest.rb
+    class DatabaseManifest < Moonshine::Manifest:Rails
+
+      include Moonshine::Omnipitr
+
+      # other recipes and configuration omitted
+
+      # tell DatabaseManifest to use the omnipitr recipe
+      # omnipitr should only be setup on the slave database server
+      if Facter.hostname =~ /^db2/
+        recipe :omnipitr
+      end
+    end
+
+Moonshine::Omnipitr requires your S3 bucket, access key, and secret key so that it can upload the backup files to S3.
+
+## Configuration Options
+
+Here's some other omnipitr configuration options you may be interested in.
+
+ * `:s3_bucket`: the S3 bucket to store backups
+ * `:s3_prefix`: an optional prefix to prepend to backup files (allows backups to be store in a subdirectory of the bucket)
+ * `:s3_key`: your S3 access key
+ * `:s3_secret`: your S3 secret key
+
+These are namespaced under `:omnipitr`. They can be configured a few ways:
+
+    # in global config/moonshine.yml
+    :omnipitr:
+      :s3_bucket: 'your-bucket'
+      :s3_prefix: 'backups/'
+      :s3_key: 'YOUR_ACCESS_KEY'
+      :s3_secret: 'YOUR_SECRET_KEY'
+
+    # in stage-specific moonshine.yml,
+    # config/moonshine/staging.yml and config/moonshine/production.yml
+    :omnipitr:
+      :s3_bucket: 'your-bucket'
+      :s3_prefix: 'backups/'
+      :s3_key: 'YOUR_ACCESS_KEY'
+      :s3_secret: 'YOUR_SECRET_KEY'
+
+    # `configure` call in app/manifests/database_manifest.rb
+    configure :omnipitr => {
+      :s3_bucket => 'your-bucket',
+      :s3_prefix => 'backups/',
+      :s3_key    => 'YOUR_ACCESS_KEY',
+      :s3_secret => 'YOUR_SECRET_KEY'
+    }
---
+++ lib/moonshine/omnipitr.rb
@@ -0,0 +1,78 @@
+module Moonshine
+  module Omnipitr
+    def omnipitr_template_dir
+      Pathname.new(__FILE__).dirname.join('omnipitr', 'templates')
+    end
+
+    def omnipitr(options = {})
+      omnipitr_version = options[:version] || '1.2.0'
+
+      package 'wget', :ensure => :installed
+      exec 'download omnipitr tarball',
+        :command => "wget https://github.com/omniti-labs/omnipitr/archive/v#{omnipitr_version}.tar.gz -O /usr/local/src/omnipitr-#{omnipitr_version}.tar.gz",
+        :creates => "/usr/local/src/omnipitr-#{omnipitr_version}.tar.gz",
+        :require => package('wget')
+
+      package 'aws-sdk',
+        :ensure => :installed,
+        :provider => :gem
+
+      %w(perl perl-modules).each do |p|
+        package p, :ensure => :installed, :before => exec('install omnipitr')
+        end
+
+      exec 'install omnipitr',
+        :command => "tar xzvf /usr/local/src/omnipitr-#{omnipitr_version}.tar.gz &&  mkdir -p /opt/OmniPITR && cp -R /tmp/omnipitr-1.2.0/* /opt/OmniPITR/ && rm -fr /tmp/omnipitr-1.2.0",
+        :cwd => '/tmp',
+        :unless => "grep version /opt/OmniPITR/META.json | grep #{omnipitr_version}",
+        :require => exec('download omnipitr tarball')
+
+      file '/usr/local/bin/omnipitr-backup-s3upload',
+        :ensure => :present,
+        :content => template(omnipitr_template_dir.join('omnipitr-backup-s3upload'), binding),
+        :require => package('aws-sdk'),
+        :mode => '755'
+
+      file '/var/lib/postgresql/.pgpass',
+        :ensure => :present,
+        :content => template(omnipitr_template_dir.join('pgpass'), binding),
+        :mode => '600',
+        :group => 'postgres',
+        :owner => 'postgres'
+
+      file '/var/log/omnipitr',
+        :ensure => :directory,
+        :group => 'postgres',
+        :owner => 'postgres'
+
+      file '/etc/omnipitr-backup-slave.conf',
+        :ensure => :present,
+        :content => template(omnipitr_template_dir.join('omnipitr-backup-slave.conf'), binding),
+        :mode => '644',
+        :group => 'postgres',
+        :owner => 'postgres'
+
+      if options[:cron] != false
+        options[:cron] ||= {}
+        cron 'omnipitr_backup_slave',
+          :command => '/opt/OmniPITR/bin/omnipitr-backup-slave --config-file /etc/omnipitr-backup-slave.conf',
+          :minute => options[:cron][:minute] || '30',
+          :hour => options[:cron][:hour] || '02',
+          :monthday => options[:cron][:monthday] || '*',
+          :month => options[:cron][:month] || '*',
+          :weekday => options[:cron][:weekday] || '*',
+          :user => 'postgres',
+          :require => [
+            exec('install omnipitr'),
+            file('/usr/local/bin/omnipitr-backup-s3upload'),
+            file('/var/lib/postgresql/.pgpass'),
+            file('/var/log/omnipitr'),
+            file('/etc/omnipitr-backup-slave.conf')
+          ]
+      else
+        cron 'omnipitr_backup_slave', :command => 'true', :ensure => :absent
+      end
+    end
+
+  end
+end
